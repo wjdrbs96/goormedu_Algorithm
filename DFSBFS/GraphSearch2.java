248a9954eb47a94484c757bdf4dcf32be550f8ce
최정균 <wjdrbs966@naver.com>
2020-01-31 00:56:37 +0900

    DFS / BFS 연습중

--- DFSBFS/GraphSearch2.java
+++ DFSBFS/GraphSearch2.java
@@ -1,10 +1,8 @@
 package goormedu_Algorithm.DFSBFS;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
-public class GraphSearch1 {
+public class GraphSearch2 {
     public static int V;
 
     public static int P;
@@ -19,9 +17,9 @@ public class GraphSearch1 {
 
     public static void DFS(int v) {
         visited[v] = true;
-        System.out.print(v);
 
-        for (int w = 1; w <= V; ++w) {
+        System.out.print(v);
+        for (int w = 1; w <= V; ++w) {;
             if (!visited[w] && adj[v][w] == 1) {
                 System.out.print("-");
                 DFS(w);
@@ -29,23 +27,7 @@ public class GraphSearch1 {
         }
     }
 
-    public static void BFS(int v) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(v);
-        visit[v] = true;
-        System.out.print(v);
 
-        while (!q.isEmpty()) {
-            v = q.poll();
-            for (int w = 1; w <= V; ++w) {
-                if (!visit[w] && adj[v][w] == 1) {
-                    visit[w] = true;
-                    System.out.print("-" + w);
-                    q.offer(w);
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -62,8 +44,13 @@ public class GraphSearch1 {
 
         DFS(1);
         System.out.println();
-        BFS(1);
+        int i = 0;
 
+        for (i = 0; i < V - 1; ++i) {
+            System.out.print(i+1 + "-");
+        }
+
+        System.out.println(i + 1);
 
     }
 }
